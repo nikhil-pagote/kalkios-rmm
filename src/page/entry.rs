efc67a20127c60886fa6c0643164804e91a32e70
4lDO2 <4lDO2@protonmail.com>
2022-07-17 13:30:44 +0200

    Use wrapped flags for PageEntry.

--- src/page/entry.rs
+++ src/page/entry.rs
@@ -2,6 +2,7 @@ use core::marker::PhantomData;
 
 use crate::{
     Arch,
+    PageFlags,
     PhysicalAddress,
 };
 
@@ -28,8 +29,13 @@ impl<A: Arch> PageEntry<A> {
     }
 
     #[inline(always)]
-    pub fn flags(&self) -> usize {
-        self.data & A::ENTRY_FLAGS_MASK
+    pub fn flags(&self) -> PageFlags<A> {
+        unsafe { PageFlags::from_data(self.data & A::ENTRY_FLAGS_MASK) }
+    }
+    #[inline(always)]
+    pub fn set_flags(&mut self, flags: PageFlags<A>) {
+        self.data &= !A::ENTRY_FLAGS_MASK;
+        self.data |= flags.data();
     }
 
     #[inline(always)]
